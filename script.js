a72c32d9731921a45bf35c91b1588dde6133a20a
Chance Thomas <cbthomas@mail.umhb.edu>
2024-02-28 14:31:16 -0600

    chance

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function calculateScore() {
     let score = 0;
-    // Example scoring logic for one question
-    if (document.getElementById('js3').checked) {
+    
+    // Question 1: What is??
+    const sliderValue = parseInt(document.getElementById('slider').value);
+    
+    // Answer is not determined yet so it will be blank
+    if (sliderValue >= 10 && sliderValue<=20) {
         score++;
     }
-    document.getElementById('result').textContent = "Your score is: " + score;
+}
+
+    //Question 2: Java questions
+    const q20ptions = document.querySelectorAll('input[name="q2"]:checked');
+    if  (q20ptions.length === 1 && q20ptions[0].value === "false") {
+        score++;
+    }
+    document.getElementById(result).textContent = "Your score is: " + score + "";
 }
